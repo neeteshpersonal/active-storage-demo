864fe91ed17dade6d83ccc04da0e2c6adac378eb
Neetesh gupta <neetes.gupta95@gmail.com>
2018-07-27 15:20:30 +0530

    Routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :signup, only: [:index, :create]
+
+  root "signup#index"
 end
