b33c6f7c963cfca9d0577a36a242a8c336cf6f0f
Ayushi Rajguru <ayushirajguru27@gmail.com>
2018-08-13 16:35:53 +0530

    display image

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -9,6 +9,10 @@ class SignupController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+  	@user = User.find(params[:id])
+  end
+
   private
 	  def user_params
 	    params.require(:user).permit(:name, :avatar)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :signup, only: [:index, :create]
+  resources :signup, only: [:index, :create, :show]
+
 
   root "signup#index"
 end
